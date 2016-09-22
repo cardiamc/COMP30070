d2d8854a20edaa35e38f4ad0e6cdeeee6883c2fc
cardiamc <cardiamc@gmail.com>
2016-09-22 18:03:43 +0100

    Update person.rb

--- person.rb
+++ person.rb
@@ -2,14 +2,14 @@
 
 class Person
  def initialize(name)
-  @name=John
+  @name=name
   @age=0
  end
  def to_s
-  puts "#{name} is #{age} years old"
+  puts "#{@name} is #{@age} years old"
  end
 end
 
-my_person=Person.new
-my_person.tos_s
+my_person=Person.new('John')
+my_person.to_s
 
