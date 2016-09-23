bedca8959b26e0cc3d076f7e2d1df160cea6ee1f
cardiamc <cardiamc@gmail.com>
2016-09-23 10:05:17 +0200

    Update person.rb

--- person.rb
+++ person.rb
@@ -7,9 +7,21 @@ class Person
  end
  def to_s
   puts "#{@name} is #{@age} years old"
+  if (14..17).include?(@age)
+  	puts "#{@name} is a teenager"
+  end
+  if (18..80).include?(@age)
+  	puts "#{@name} is an adult"
+  end
+ end
+ def increment_age
+  @age=@age+1
  end
 end
 
 my_person=Person.new('John')
 my_person.to_s
-
+for i in 0..19
+ my_person.increment_age
+end
+my_person.to_s
